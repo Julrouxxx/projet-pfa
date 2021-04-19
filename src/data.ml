2e52abdf67887fab352114ef910e971da41c11ee
Julrouxxx <julien.labbe@u-psud.fr>
2021-04-19 20:16:59 +0200

    Remove write_data (does not work) Add init_data and create_level to generate a level (WIP)

--- src/data.ml
+++ src/data.ml
@@ -1,11 +1,8 @@
 type obstacle = {time: float; t: string; x: int; y: int; direction: int; speed: float};;
-type level = {effect: string list ; obstacles: obstacle list };;
+type level = {red: int; green: int; blue: int ; obstacles: obstacle list };;
 let list = ref [];;
 
-let rec load_effects acc =
-    match acc with
-    effect :: effects -> effect::(load_effects effects)
-    | [] -> acc ;;
+
 let load_obstacles_aux acc =
     match acc with
     [time; t; x; y; direction; speed] -> {time=(float_of_string time); t=t; x=(int_of_string x); y=(int_of_string y); direction=(int_of_string direction); speed=(float_of_string speed)}
@@ -17,7 +14,7 @@ let rec load_obstacles acc =
 ;;
 let load_line line =
    match String.split_on_char ',' line with
-       [effects; obstacles] -> {effect=(load_effects (String.split_on_char ';' effects)); obstacles=(load_obstacles (String.split_on_char '@' obstacles))}
+       [colors; obstacles] -> {red=  int_of_string (List.nth (String.split_on_char ';' colors) 0); green=  int_of_string (List.nth (String.split_on_char ';' colors) 1); blue= int_of_string (List.nth (String.split_on_char ';' colors) 2); obstacles=(load_obstacles (String.split_on_char '@' obstacles))}
        | _ -> failwith "Error 1";;
 let load_data file =
     let ic = open_in file in
@@ -34,13 +31,18 @@ let load_data file =
 let size_level () =
     let s = List.length !list in
     s;;
-let _size_obstacle level =
-    let l = List.nth !list level in
-    List.length l.effect;;
-let _get_effect level e =
+let _get_color_red level =
      let l  = List.nth !list level in
-     let o = List.nth (l.effect) e in
+     let o = l.red in
      o;;
+let _get_color_green level =
+    let l  = List.nth !list level in
+    let o = l.green in
+    o;;
+let _get_color_blue level =
+    let l  = List.nth !list level in
+    let o = l.blue in
+    o;;
 let _size_obstacle level =
     let l = List.nth !list level in
     List.length l.obstacles;;
@@ -49,16 +51,10 @@ let _get_obstacle level o =
     let o = List.nth (l.obstacles) o in
     o;;
 
-let write_data file =
-    let oc = open_out file in
-    begin
-    (*Level 1*)
-    Printf.fprintf oc "//,";
-    for i = 1 to 100  do
-    if i != 1 then begin
-    Printf.fprintf oc "@";
-    end;
-        let random_direction = 1+(Random.int 4) in
+let str = ref "";;
+
+let create_obstacle i =
+ let random_direction = 1+(Random.int 4) in
         let random_int_X = ref 0 in
         let random_int_Y = ref 0 in
         if random_direction == Globals.direction_down || random_direction == Globals.direction_up then begin
@@ -71,9 +67,31 @@ let write_data file =
           if random_direction == Globals.direction_left then
             random_int_X := 31;
         end;
-             Printf.fprintf oc "%f|OBS|%d|%d|%d|%f"  (500.0 *. (float_of_int i)) !random_int_X !random_int_Y random_direction 50.0;
-             (*if i == 100 then begin Printf.fprintf oc "\n"; end;*)
-      done;
+ let t = string_of_float (500.0 *. (float_of_int i)) ^ "|OBS|" ^ string_of_int !random_int_X ^ "|" ^ string_of_int !random_int_Y  ^ "|" ^ string_of_int random_direction ^ "|" ^ string_of_float 50.0 ^ "@" in
+ t;;
 
-    close_out oc;
+
+
+
+let rec init_data r g b =
+    begin
+
+    str := "//" ^ string_of_int r ^ ";" ^ string_of_int 255 ^ ";" ^ string_of_int 255^ ",";
     end;
+and
+create_level l =
+match l with
+  1 -> begin init_data 255 255 255;
+  for i = 1 to 100 do
+    str := !str ^ create_obstacle i;
+  done;
+      str := String.sub  !str 0 (String.length !str - 1);
+
+        Gfx.debug !str;
+  end;
+
+| 2 -> (); (* do level 2 *)
+| _ -> (); (* unknown *)
+
+
+;;
--- src/data.mli
+++ src/data.mli
@@ -6,15 +6,18 @@ type obstacle = {
   direction : int;
   speed : float;
 }
-type level = { effect : string list; obstacles : obstacle list; }
+type level = {red: int; green: int; blue: int ; obstacles: obstacle list }
 val list : level list ref
-val load_effects : 'a list -> 'a list
 val load_obstacles_aux : string list -> obstacle
 val load_obstacles : string list -> obstacle list
 val load_line : string -> level
 val load_data : string -> level list
 val size_level : unit -> int
-val _get_effect : int -> int -> string
+val _get_color_red : int -> int
+val _get_color_green : int -> int
+val _get_color_blue : int -> int
+
 val _size_obstacle : int -> int
 val _get_obstacle : int -> int -> obstacle
-val write_data : string -> unit
+val init_data : int -> int -> int -> unit
+val create_level: int -> unit
